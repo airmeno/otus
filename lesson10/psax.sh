7f990ce97ca4701091acbc08c9b080db2dd17d7c
Airmeno <meno@list.ru>
2020-12-06 16:10:03 +0300

    processes

--- lesson10/psax.sh
+++ lesson10/psax.sh
@@ -19,7 +19,6 @@ a=$(ls -l /proc | awk '{print $9}' | grep -s "^[0-9]*[0-9]$"| sort -n)
 # Перебираем по очереди выборку и формируем построчно столбцы согласно заголовку из файла stat. Любые ошибки при выводе игнорируем.
 for pid in $a;
 do
-
 tty=$(cat 2>/dev/null /proc/$pid/stat | awk '{print $7}')
 stat=$(cat 2>/dev/null /proc/$pid/stat | awk '{print $3}')
 utime=$(cat 2>/dev/null /proc/$pid/stat | awk '{print $14}')
@@ -28,11 +27,18 @@ cmd=$(cat 2>/dev/null /proc/$pid/stat | awk '{print $2}')
 # вывод информации аргуметов запуска процесса
 cmd_arg=$(cat 2>/dev/null /proc/$pid/cmdline | awk '{print $0}')
 
+# определем терминал/псевдотерминал (tty_ | pts/_)
+if [[ "$tty" -eq 0 ]]
+then tty='?'
+else tty=$(ls -la /proc/$pid/fd/ | grep -m 1 'pts\|tty' | awk '{print $11}' | sed 's/\/*dev\///')
+fi
+
 # Считаем процессорное время для столбка TIME
 ttime=$(((utime + stime) / clk_tck)) 
 ftime=$(secconverttime $ttime)
 
 # вывод результатов
-echo "$pid | $tty | $stat | $fttime | $cmd $cmd_arg" | column -t  -s '|'
+echo "$pid | $tty | $stat | $ftime | $cmd $cmd_arg" | column -t  -s '|'
 
 done
+
